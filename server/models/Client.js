705e1a71dc43f137c49f6d83df81e87f270901be
Jeremy-Nourri <nourri.jer@gmail.com>
2024-03-26 11:12:43 +0100

    feat: ajout du model Personne, Client et livreur

---
+++ server/models/Client.js
@@ -0,0 +1,27 @@
+const Personne = require("./Personne");
+
+module.exports = (sequelize) => {
+  class Client extends Personne {}
+
+  Client.init(
+    {
+      clientId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      personneId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Client",
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
+
+  return Client;
+};
---
+++ server/models/Personne.js
@@ -0,0 +1,42 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Personne extends Model {}
+
+  Personne.init(
+    {
+      personneId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nom: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      prenom: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      telephone: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Personne",
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
+  return Personne;
+};
