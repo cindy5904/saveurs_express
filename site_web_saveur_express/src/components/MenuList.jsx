c98bc922dbf53f4b9df810ae28921bd25d1d3d2e
JabKh <jaber.khaloua@hotmail.com>
2024-03-27 12:16:43 +0100

    modification du MenuList

--- site_web_saveur_express/src/components/MenuList.jsx
+++ site_web_saveur_express/src/components/MenuList.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import MenuCard from './MenuCard'; // Importe le composant MenuCard
-import './style/menuList.css'; // Importe le fichier CSS pour le style
+import React, { useState, useRef } from 'react';
+import MenuCard from './MenuCard';
+import './style/menuList.css';
 
 const MenuList = ({ menus }) => {
+  const filterInputRef = useRef(null); // Référence pour la zone de saisie
+
+  // Fonction de mise à jour de l'état avec le critère de filtrage saisi par l'utilisateur
+  const handleFilterChange = () => {
+    const filterValue = filterInputRef.current.value; // Récupère la valeur de la zone de saisie
+    setFilterCriteria(filterValue); // Met à jour l'état avec la valeur saisie
+  };
+
+  const [filterCriteria, setFilterCriteria] = useState(''); // État pour stocker les critères de filtrage
+
+  // Filtre les menus en fonction du critère de filtrage (nom du menu)
+  const filteredMenus = menus.filter((menu) =>
+    menu.nom.toLowerCase().includes(filterCriteria.toLowerCase())
+  );
+
   return (
     <div className="menu-list">
-      {menus.map((menu) => (
-        <MenuCard key={menu.id} menu={menu} /> // Affiche MenuCard pour chaque menu dans la liste
+      {/* Zone de saisie pour le filtrage par nom */}
+      <input
+        type="text"
+        placeholder="Rechercher un menu par nom..."
+        ref={filterInputRef} // Associe la référence à la zone de saisie
+        onChange={handleFilterChange} // Appelle la fonction de mise à jour lors de tout changement
+      />
+
+      {/* Affichage des cartes de menu filtrées */}
+      {filteredMenus.map((menu) => (
+        <MenuCard key={menu.id} menu={menu} />
       ))}
     </div>
   );
