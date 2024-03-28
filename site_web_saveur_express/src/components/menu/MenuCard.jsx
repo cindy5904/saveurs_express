30e56d7ab924b1fb85333c995f4642d2d5c46582
Cindy Telmon <telcindy59@gmail.com>
2024-03-28 20:56:39 +0100

    ajout au panier ok, supprission de la quantité et ajout d'une quantité ok, calcul des prix ok

--- site_web_saveur_express/src/components/menu/MenuCard.jsx
+++ site_web_saveur_express/src/components/menu/MenuCard.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import './style/menuCard.css';
 import { addPanier } from '../Global/globalSlice';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 const MenuCard = ({ data }) => { 
   const dispatch = useDispatch();
-
+  const panier = useSelector(state => state.global.panier);
+  
   const handleAddToCart = () => {
-      dispatch(addPanier({ id: data.id, quantity: 1, nom: data.nom, prix: data.prix, image: data.image}));
+    const existingItem = panier.find(item => item.id === data.id);
+    if (existingItem) {
+      dispatch(addPanier({ id: existingItem.id, quantity: existingItem.quantity + 1 }));
+    } else {
+      dispatch(addPanier({ id: data.id, quantity: 1, nom: data.nom, prix: data.prix, 
+        image: data.image, description: data.description }));
+    }
   };
+
+
   return (
     <div id="card-menu" className="card">
       <div className="container-image">
